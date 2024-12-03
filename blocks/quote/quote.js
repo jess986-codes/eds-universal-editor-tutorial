465970b0821983e687ca6338ae880ba548a722c7
Jessica Yuen <jeyuen@deloitte.com.au>
2024-12-03 18:20:00 +1100

    add console logs to see what is in quote js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,10 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
+  console.log(`block object is ${block}`);
+  console.log(`quote wrapper object is ${quoteWrapper}`);
+  console.log(`quote wrapper text content is ${quoteWrapper.textContent}`);
+
   const blockquote = document.createElement("blockquote");
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
