ffa9648c6fd00fb49822584080951cb9a4ee1016
Jessica Yuen <jeyuen@deloitte.com.au>
2024-12-03 18:24:08 +1100

    remove quote js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,1 @@
-export default function decorate(block) {
-  const [quoteWrapper] = block.children;
 
-  const blockquote = document.createElement("blockquote");
-  blockquote.textContent = `block object is ${block}`;
-  quoteWrapper.replaceChildren(blockquote);
-}
